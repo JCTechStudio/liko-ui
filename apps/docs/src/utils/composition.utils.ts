ed0156e1d6bdbabf3e5cd3a3ffa404c2e5d2d99b
José Carlos Fatjó García <jcfatjo@jctechstudio.com>
2026-02-22 18:05:35 +0100

    feat(docs): add box example pages for en and es langs

---
+++ apps/docs/src/utils/composition.utils.ts
@@ -0,0 +1,9 @@
+export const compositionData = {
+  imageUrl: "https://bit.ly/2Z4KKcF",
+  imageAlt: "Rear view of modern home with pool",
+  beds: 3,
+  title: "Modern home in city center in the heart of historic Los Angeles",
+  formattedPrice: "$435",
+  reviewCount: 34,
+  rating: 4.5,
+};
